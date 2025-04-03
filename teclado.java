208983b486acf50b9cd86d5ccc78bb8a656159ec
Pablo Hernández <141796387+PabloHM23@users.noreply.github.com>
2025-04-02 17:16:22 -0700

    Add files via upload

---
+++ teclado.java
@@ -0,0 +1,43 @@
+package tecladoRGB;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class teclado {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					teclado window = new teclado();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public teclado() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
